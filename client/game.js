08fd6cce2eef91454d17b56b7955a75a9f076c01
NyleVII <stephenantymis@gmail.com>
2017-09-27 20:29:55 -0600

    Loaded a bunch of new cards

--- client/game.js
+++ client/game.js
@@ -14,11 +14,15 @@ function Game(renderer)
 	
 	this.sprite_player_creature = new PIXI.Sprite();
 	this.sprite_player_creature.anchor.set(0, 1);
+	this.sprite_player_creature.width = 300;
+	this.sprite_player_creature.height = 300;
 	this.sprite_player_creature.y = renderer.height;
 	this.stage.addChild(this.sprite_player_creature);
 	
 	this.sprite_opponent_creature = new PIXI.Sprite();
-	this.sprite_opponent_creature.anchor(1, 0);
+	this.sprite_opponent_creature.anchor.set(1, 0);
+	this.sprite_opponent_creature.width = 300;
+	this.sprite_opponent_creature.height = 300;
 	this.sprite_opponent_creature.x = renderer.width;
 	this.stage.addChild(this.sprite_opponent_creature);
 	
@@ -42,8 +46,10 @@ Game.prototype.state_set = function(state)
 	this.sprite_player_creature.texture = PIXI.loader.resources[state.player.creature.id + "_player"].texture;
 	this.sprite_opponent_creature.texture = PIXI.loader.resources[state.opponent.creature.id + "_opponent"].texture;
 	
-	for (let i = 0; i < state.player.effecthand.length; ++i)
-		this.sprite_effectcards[i].texture = PIXI.loader.resources[state.player.effecthand[i].id + "_card"].texture;
+	for (let i = 0; i < state.player.effecthand.length; ++i){
+		console.log(state.player.effecthand[i] + "_card");
+		this.sprite_effectcards[i].texture = PIXI.loader.resources[state.player.effecthand[i] + "_card"].texture;
+	}
 };
 
 Game.prototype.render = function()
--- client/render.js
+++ client/render.js
@@ -19,18 +19,29 @@ document.addEventListener("DOMContentLoaded", function()
 
 	PIXI.loader.add([{name: "mewtwo_player", url:"assets/images/mewtwo/mewtwo_player.gif"},
 					{name: "mewtwo_opponent", url:"assets/images/mewtwo/mewtwo_opponent.gif"},
-					{name: "mewtwo_card", url:"assets/images/mewtwo/mewtwo_card.png"},
+					{name: "mewtwo_card", url:"assets/images/cards/creatures/mewtwo_card.png"},
 					{name: "gyarados_player", url:"assets/images/gyarados/gyarados_player.gif"},
 					{name: "gyarados_opponent", url:"assets/images/gyarados/gyarados_opponent.gif"},
-					{name: "gyarados_card", url:"assets/images/gyarados/gyarados_card.png"},
-					{name: "cardBack", url:"assets/images/cards/cardBack.png"}]).load(function()
+					{name: "gyarados_card", url:"assets/images/cards/creatures/gyarados_card.png"},
+					{name: "cardBack", url:"assets/images/cards/cardBack.png"},
+					{name: "effect1", url:"assets/images/cards/effects/17.png"},
+					{name: "effect2", url:"assets/images/cards/effects/24.png"},
+					{name: "effect3", url:"assets/images/cards/effects/26.png"},
+					{name: "effect4", url:"assets/images/cards/effects/27.png"},
+					{name: "effect5", url:"assets/images/cards/effects/36.png"},
+					{name: "effect6", url:"assets/images/cards/effects/63.png"},
+					{name: "effect7", url:"assets/images/cards/effects/74.png"},
+					{name: "effect8", url:"assets/images/cards/effects/91.png"},
+					{name: "effect9", url:"assets/images/cards/effects/99.png"},
+					{name: "effect10", url:"assets/images/cards/effects/109.png"}
+				]).load(function()
 	{
 		game = new Game(renderer);
 		game.state_set({
 			player:
 			{
 				creature: {id: "gyarados"},
-				effecthand: ["mewtwo"]
+				effecthand: ["mewtwo", "gyarados","mewtwo","gyarados","mewtwo"]
 			},
 			opponent:
 			{
