745df281eb07316ba3f93f594594809e0e8581e0
Shawn Sutherland <shawncsutherland@gmail.com>
2017-10-19 20:13:08 -0600

    Some server game logic i think

--- server/game/game.js
+++ server/game/game.js
@@ -15,6 +15,7 @@ function init_player(name, deck)
 	
 	return {
 		name: name,
+		mulligan: false,
 		energy_max: 2,
 		energy_current: 0,
 		creatures: [],
@@ -42,8 +43,12 @@ function Game(id_deck1, id_deck2)
 		{
 			game.state =
 			{
-				player1: init_player(players[0].name, decks[0]),
-				player2: init_player(players[1].name, decks[1]),
+				players:
+				[
+					init_player(players[0].name, decks[0]),
+					init_player(players[1].name, decks[1])
+				],
+				phase: "mulligan",
 				player_current: 0,
 				turn: 0,
 				effects: []
@@ -51,5 +56,57 @@ function Game(id_deck1, id_deck2)
 		});
 	});
 }
+
+
+Game.prototype.processes =
+{
+	mulligan:
+	{
+		mulligan: function(player, action)
+		{
+			if (!player.mulligan)
+			{
+				const hand = [];
+				for (let i = 0; i < action.cards.length; ++i)
+					hand.push(player.hand[action.cards[i]]);
+				player.hand = hand;
+				player.mulligan = true;
+			}
+			
+			// start game
+			if (this.players[0].mulligan && this.players[1].mulligan)
+				this.phase = "main";
+		}
+	},
+	
+	main:
+	{
+		card: function(player, action)
+		{
+			const id_card = player.hand[action.index];
+			
+			// TODO(shawn): get card data from data store
+			
+			// TODO(shawn): send "invalid" message to client
+			if (card.cost > player.energy_current)
+				return;
+			
+			// deduct card cost from player energy
+			player.energy_current -= card.cost;
+			
+			// remove card from hand
+			player.hand.splice(action.index, 1);
+			
+			// TODO(shawn): resolve card effect
+		}
+	}
+};
+
+Game.prototype.process = function(index_player, action)
+{
+	const process = this.processes[this.phase][action.type];
+	if (process !== undefined)
+		process.call(this, this.players[index_player], action);
+};
 
 module.exports = Game;
